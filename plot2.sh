7a173a757e9e6df912d267ce4be509771e017b33
phplego <phplego@gmail.com>
2022-07-08 18:59:30 +0300

    pause -1

--- plot2.sh
+++ plot2.sh
@@ -1,5 +1,6 @@
 gnuplot -e "
 plot 'plot2.data' using 1 title 'Summary Error' with points pointsize 0.5, \
      'plot2.data' using 2 title 'Output Error'  with points pointsize 0.5;
-" --persist
+pause -1
+"
 
