82f22244e4cb604a49ca9144aba8aac1ac12f75b
phplego <phplego@gmail.com>
2022-07-24 01:09:40 +0300

    tests moved to 'test' directory

---
+++ test/test_run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $1 -ne '' ]]
+then
+    # if script argument is not empty, use it
+    TESTNAME=$1
+else
+    # ask user otherways
+    echo "Enter test name (number or string):"
+    read TESTNAME
+fi 
+
+
+file="test_$TESTNAME"
+
+# build
+g++ "$file.cpp" -o $file.out -I../lib
+
+# if no error then run binary
+if [[ $? -eq 0 ]]
+then
+    ./$file.out $2 $3 $4
+fi
+
+
