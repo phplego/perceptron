a130f706bbcfa5f73f9dd41c7313a21c2cd45ffa
phplego <phplego@gmail.com>
2022-07-23 04:15:34 +0300

    add parse config test

--- test_5.cpp
+++ test_5.cpp
@@ -1,15 +1,33 @@
-#include <cstdio>
-#include <cmath>
+//#include <cstdio>
+#include <string>
+//#include <iostream>
+#include <sstream>
+//#include <cmath>
+#include <fstream>
 
 #include "definitions.h"
 
 
+void store_line(std::string key, std::string value){
+    pf(_BLUE "'%s' = '%s'\n" _RST, key.c_str(), value.c_str());
+}            
 
 int main(int argc, char * argv []) 
 { 
-    pf_green("Expotential func..\n");
+    pf_green("Parse config..\n");
 
-    for(int i = -10; i <= 10; i++){
-        pf("exp(%.1f) = %f\n", (float)i, exp((float)i));
+    std::ifstream is_file("test_5.cfg");
+
+    std::string line = "";
+    while( std::getline(is_file, line) )
+    {
+        std::istringstream is_line(line);
+        std::string key;
+        if( std::getline(is_line, key, '=') )
+        {
+            std::string value;
+            if( std::getline(is_line, value) ) 
+            store_line(key, value);
+        }
     }
 }
