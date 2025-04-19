a2c2a994f4f423efea54045c52029644384fe0be
dub <phplego@gmail.com>
2025-04-20 04:48:06 +0700

    add bash tag

--- build_and_run_gui.sh
+++ build_and_run_gui.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 g++ `pkg-config --cflags gtk+-3.0` gui.cpp `pkg-config --libs gtk+-3.0` -o gui.out -I./lib
 
 
--- build_and_run_main.sh
+++ build_and_run_main.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 g++ main.cpp -o main.out -I./lib
 
 if [[ $? -eq 0 ]]
